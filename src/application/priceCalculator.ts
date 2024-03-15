00d465c02ca87db50fbc5889c4add2b4c629b6cc
Thomas Coopman <thomas.coopman@gmail.com>
2024-03-15 08:53:22 +0100

    session 1: trivial solution first scenario

--- src/application/priceCalculator.ts
+++ src/application/priceCalculator.ts
@@ -1,7 +1,7 @@
 export default class PriceCalculatorService {
   calculate(visit: object) {
     return {
-      price_amount: 1,
+      price_amount: 0,
       price_currency: "EUR",
       person_id: (visit as any).person_id,
       visit_id: (visit as any).visit_id,
