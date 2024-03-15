f8d8d33a10024befebfa0cf9c03f9a93ba6151a6
Thomas Coopman <thomas.coopman@gmail.com>
2024-03-15 08:54:16 +0100

    session 1: easy solution second scenario

--- src/application/priceCalculator.ts
+++ src/application/priceCalculator.ts
@@ -1,7 +1,13 @@
 export default class PriceCalculatorService {
-  calculate(visit: object) {
+  calculate(visit: any) {
+    const priceAmount = visit.dropped_fractions.reduce(
+      (price: number, d: any) =>
+        price +
+        d.amount_dropped * (d.fraction_type === "Green waste" ? 0.1 : 0.15),
+      0,
+    );
     return {
-      price_amount: 0,
+      price_amount: priceAmount,
       price_currency: "EUR",
       person_id: (visit as any).person_id,
       visit_id: (visit as any).visit_id,
