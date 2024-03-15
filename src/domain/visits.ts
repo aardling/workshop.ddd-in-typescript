a8d019637d3f53e5442733266fbe144220f7676c
Thomas Coopman <thomas.coopman@gmail.com>
2024-03-15 08:58:53 +0100

    session 2: naive implementation of full exercise

--- src/domain/visits.ts
+++ src/domain/visits.ts
@@ -1,6 +1,25 @@
-type Visit = {
-  date: Date;
-};
+type PersonId = string;
+type VisitId = string;
+
+export class Visit {
+  #date: Date;
+  #visitId: string;
+  #personId: string;
+
+  constructor(date: Date, visitId: VisitId, personId: PersonId) {
+    this.#date = date;
+    this.#visitId = visitId;
+    this.#personId = personId;
+  }
+
+  inSameMonth(other: Visit) {
+    return (
+      this.#personId === other.#personId &&
+      this.#date.getFullYear() === other.#date.getFullYear() &&
+      this.#date.getMonth() === other.#date.getMonth()
+    );
+  }
+}
 
 export class Visits {
   #visits: Array<Visit> = [];
@@ -10,9 +29,8 @@ export class Visits {
   }
 
   get numberOfVisitsInCurrentMonth() {
-    const month = this.lastVisit.date.getMonth();
     return this.#visits.reduce((sum, visit) => {
-      if (visit.date.getMonth() === month) {
+      if (visit.inSameMonth(this.lastVisit)) {
         return sum + 1;
       } else {
         return sum;
